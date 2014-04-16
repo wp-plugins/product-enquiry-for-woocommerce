64905555d239d5234f0497ad2a38a8ddf6c3af90
WisdmLabs <WisdmLabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-16 12:44:44 +0000

    version 0.3 released fixed z-index issue, IE9 issue and update dashboard UI, added links for pro version
    git-svn-id: https://plugins.svn.wordpress.org/product-enquiry-for-woocommerce/trunk@894259 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- product-enquiry-for-woocommerce.php
+++ product-enquiry-for-woocommerce.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Product Enquiry for WooCommerce
 Description: Allows prospective customers or visitors to make enquiry about a product, right from within the product listing page.
-Version: 0.2.9
+Version: 0.3
 Author: WisdmLabs
 Author URI: http://wisdmlabs.com
 License: GPL2
@@ -12,22 +12,28 @@ $form_init_data = get_option( 'wdm_form_data');
 
 if(!empty($form_init_data))
 {
+    if(isset($form_init_data['show_after_summary']))
+    {
     if($form_init_data['show_after_summary'] == 1)
     {
 	//show ask button after a single product summary
-        add_action('woocommerce_after_single_product_summary', 'ask_about_product');
+        add_action('woocommerce_single_product_summary', 'ask_about_product');
     }
-
+    }
+    
+    if(isset($form_init_data['show_at_page_end']))
+    {
     if($form_init_data['show_at_page_end'] == 1)
     {
 	//show ask button at the end of the page of a single product
         add_action('woocommerce_after_single_product', 'ask_about_product');
     }
+    }
 }
 else
 {
     //show ask button after a single product summary as default
-    add_action('woocommerce_after_single_product_summary', 'ask_about_product');
+    add_action('woocommerce_single_product_summary', 'ask_about_product');
 }
 
 function ask_about_product()
@@ -51,7 +57,10 @@ function ask_about_product()
     <!-- Load JavaScript files -->
    <?php
         wp_enqueue_script("wdm-simple-modal", plugins_url("js/jquery.simplemodal.js", __FILE__), array("jquery"));
-        wp_enqueue_script("wdm-contact", plugins_url("js/contact.js", __FILE__), array("jquery"));
+        wp_enqueue_script("wdm-contact", plugins_url("js/contact.js", __FILE__), array("jquery"));	
+
+		
+
         
 	//pass parameters to contact.php file
         $wdm_translation_array = array( 'product_name'=>get_the_title(), 'contact_url' => plugins_url("data/contact.php", __FILE__), 'form_dataset' => $form_data, 'admin_email' => get_option('admin_email'), 'site_name' => get_bloginfo('name'));
@@ -64,7 +73,7 @@ add_action('admin_menu', 'create_ask_product_menu');
 function create_ask_product_menu()
 {
     //create a submenu under Woocommerce 'Products' menu
-    add_submenu_page('edit.php?post_type=product', 'Product Enquiry', 'Product Enquiry', 'manage_options', 'product-enquiry-for-woocommerce', 'add_ask_product_settings' );
+    add_submenu_page('edit.php?post_type=product', 'Product Enquiry', 'Product Enquiry', 'manage_options', 'pefw', 'add_ask_product_settings' );
 }
 
 add_action('admin_init', 'reg_form_setting' );
@@ -82,10 +91,116 @@ function add_ask_product_settings()
     wp_enqueue_script('wdm_wpi_validation', plugins_url("js/wdm-jquery-validate.js", __FILE__), array('jquery'));
     
     ?>
+    
       <div class="wrap wdm_leftwrap">
+      	<div class='wdm-pro-notification'>
+      
+      <div class='wdm-title-layer'>
+		<h4>Get The New Premium Version</h4>
+      </div> <!--wdm-title-layer ends-->
+      
+      <div class="wdm-content-layer">
+      	<div class="wdm-left-content">
+       		<img src='<?php echo plugins_url('img/PEP_new.png',__FILE__); ?>' class='wdm_pro_logo'>
+			<div class="wdm-actionBtn">
+        		<a class='wdm_upgrade_pro_link' href='http://wisdmlabs.com/woocommerce-product-enquiry-pro/' target='_blank'>UPGRADE TO PRO </a>
+            </div>
+        </div>
+        <div class="wdm-right-content">	
+        	<div class="wdm-features">
+            	<h3 class='wdm_feature_heading'>New Features In Pro Version</h3>
+               
+			   <div class='wdm-feature-list'>
+				
+						<div class="wdm-feature">
+							<span class="wdmiconfilter"></span>
+							<p>Filter enquires</p>
+						</div>
+					
+						<div class="wdm-feature">
+							<span class="wdmiconexpand"></span>
+							<p>Responsive</p>
+						</div>
+																					
+						<div class="wdm-feature">
+							<span class="wdmiconpaint-format"></span>
+							<p>Custom styling</p>
+						</div>
+					
+					<div class="clear"></div>
+				</div>
+				
+				<div class='wdm-feature-list'>
+					
+							<div class="wdm-feature">
+								<span class="wdmiconeye"></span>
+								<p>Enquiries in dashboard</p>
+							</div>
+							
+							<div class="wdm-feature">
+								<span class="wdmiconarchive"></span>
+								<p>Export enquiry records</p>
+							</div>	
+							
+							<div class="wdm-feature">
+								<span class="wdmiconbubbles"></span>
+								<p>Localization ready</p>
+							</div>
+						
+							<div class="clear"></div>
+				</div>
+				
+				<div class="wdm_coupon">
+						<p>USE THIS COUPON CODE AND GET A 25% DISCOUNT*</p>
+							<div>
+								<h1>PEPDISCOUNT25</h1>
+						</div>
+						<p>*Valid till 31st May 2014</p>
+				</div>
+            </div>
+        </div>
+        <div class='clear'></div>
+      </div>
+      <div class='clear'></div>
+	
+    </div> <!--wdm-pro-notification ends-->
+    
         <h2>Product Enquiry</h2>
-
-     <form name="ask_product_form" id="ask_product_form" method="POST" action="options.php">
+<br />
+	<?php
+	if( isset( $_GET[ 'tab' ] ) )   
+            $active_tab = $_GET[ 'tab' ];  
+	else	    
+            $active_tab = 'form';
+        
+        ?>
+            <h2 class="nav-tab-wrapper">  
+                <a href="edit.php?post_type=product&page=pefw&tab=form" class="nav-tab <?php echo $active_tab == 'form' ? 'nav-tab-active' : ''; ?>">Enquiry Settings</a>
+		<a href="edit.php?post_type=product&page=pefw&tab=entry" class="nav-tab <?php echo $active_tab == 'entry' ? 'nav-tab-active' : ''; ?>">Enquiry Details</a>
+            </h2>  
+	
+    <?php if($active_tab === 'entry'){
+	
+	?>
+	<div id='entry_dummy'>
+	    <div class="layer_parent">
+		    <div class="pew_upgrade_layer">
+			<div class="pew_uptp_cont">
+			    <p> This feature is available in the PRO version. Click below to know more.</p>
+			<a class="wdm_view_det_link" href="http://wisdmlabs.com/woocommerce-product-enquiry-pro/" target="_blank"> View Details </a>
+			</div>
+		    </div>
+		    <img src="<?php echo plugins_url('/img/entries.png', __FILE__); ?>" style='width:100%;height: 623px'/>
+	    </div>
+	    
+	</div>
+	<?php
+	 
+    }
+    else{
+	$pro = "<span title='Pro Feature' class='pew_pro_txt'> [Available in PRO] </span>";
+    ?>
+     <form name="ask_product_form" id="ask_product_form" method="POST" action="options.php" style="background: #fff; padding: 10px 15px 0 15px;">
         <?php
             settings_fields('wdm_form_options');
             $default_vals =   array('show_after_summary'=>1        
@@ -94,53 +209,163 @@ function add_ask_product_settings()
             ?>
             
       <div id="ask_abt_product_panel">
-        <p>
-            <label for="wdm_user_email"> Recipient's Email </label> <br />
-        <input type="text" class="wdm_wpi_input wdm_wpi_text email" name="wdm_form_data[user_email]" id="wdm_user_email" value="<?php echo empty($form_data['user_email']) ? get_option('admin_email') : $form_data['user_email'];?>" />
-        </p>
-        
-        <p>
-            <label for="wdm_default_sub"> Default Subject (<em> will be used if the customer does not enter a subject </em>)</label> <br />
-        <input type="text" class="wdm_wpi_input wdm_wpi_text" name="wdm_form_data[default_sub]" id="wdm_default_sub" value="<?php echo empty($form_data['default_sub']) ? 'Enquiry for a product from '.get_bloginfo('name') : $form_data['default_sub'];?>"  />
-        </p>
-        
-	<p>
-            <label for="custom_label"> Label for enquiry button </label> <br />
+	<fieldset>
+	    <?php echo '<legend>'. __("Emailing Information",'pep_text_domain').'</legend>'; ?>
+	<div class="fd">
+	<div class='left_div'>
+            <label for="wdm_user_email"> Recipient's Email </label> 
+	</div>
+	<div class='right_div'>
+	    <input type="text" class="wdm_wpi_input wdm_wpi_text email" name="wdm_form_data[user_email]" id="wdm_user_email" value="<?php echo empty($form_data['user_email']) ? get_option('admin_email') : $form_data['user_email'];?>" />
+	    <span class='email_error'> </span>
+	</div>
+	<div class='clear'></div>
+	</div >
+	<div class="fd">
+	<div class='left_div'>
+	    <label for="wdm_default_sub"> Default Subject </label>
+	</div>
+	<div class='right_div'>		
+	    <input type="text" class="wdm_wpi_input wdm_wpi_text" name="wdm_form_data[default_sub]" id="wdm_default_sub" value="<?php echo empty($form_data['default_sub']) ? 'Enquiry for a product from '.get_bloginfo('name') : $form_data['default_sub'];?>"  />
+        <br>
+	    <?php echo '<em> Will be used if the customer does not enter a subject </em>'; ?>
+	</div>
+	<div class='clear'></div>
+	</div>
+        </fieldset>
+	<br/>
+	    <fieldset>
+	
+	 <?php echo '<legend>'. __("Form Options",'pep_text_domain').'</legend>'; ?>
+	<div class="fd">
+			<div class='left_div'>
+            <label for="custom_label"> Button-Text for enquiry button </label> 
+	    </div>
+			<div class='right_div'>
             <input type="text" class="wdm_wpi_input wdm_wpi_text" name="wdm_form_data[custom_label]" value="<?php echo empty($form_data['custom_label']) ? 'Make an enquiry for this product' : $form_data['custom_label'];?>" id="custom_label"  />
-        </p>
-	
-        <p>
+        </div>
+			<div class='clear'></div>
+		</div>
+	<div class="fd">
+			<div class='left_div'>
+	    <label> Enquiry Button Location </label>
+	    </div>
+			<div class='right_div'>
             <input type="checkbox" class="wdm_wpi_input wdm_wpi_checkbox" name="wdm_form_data[show_after_summary]" value="1" <?php checked( 1, $form_data["show_after_summary"] );?> id="show_after_summary" />
-	    <label for="show_after_summary"> Show ask button after single product summary </label>
-        </p>
+	    
+	    <label for="show_after_summary"> After single product summary </label>
+	    <br />
+	    <input type="checkbox" class="wdm_wpi_input wdm_wpi_checkbox" name="wdm_form_data[show_at_page_end]" value="1" <?php checked( 1, $form_data["show_at_page_end"] );?> id="show_at_page_end" />
+	   
+	    <label for="show_at_page_end"> At the end of single product page </label>
+	    
+        </div>
+	    <div class='clear'></div>
+	</div>
         
-        <p>
-            <input type="checkbox" class="wdm_wpi_input wdm_wpi_checkbox" name="wdm_form_data[show_at_page_end]" value="1" <?php checked( 1, $form_data["show_at_page_end"] );?> id="show_at_page_end" />
-	    <label for="show_at_page_end"> Show ask button at the end of single product page </label>
-        </p>
-	
-        <p>
+	<div class="fd">
+	    <div class='left_div'>
+		<label> Enable sending an email copy </label>
+		 </div>
+	    <div class='right_div'>
             <input type="checkbox" class="wdm_wpi_input wdm_wpi_checkbox" name="wdm_form_data[enable_send_mail_copy]" value="1" <?php checked( 1, $form_data["enable_send_mail_copy"] );?> id="enable_send_mail_copy" />
+	  
 	    <label for="enable_send_mail_copy"> Enable option on the enquiry form to send an email copy to customer </label>
-        </p>
+        </div>
+			<div class='clear'></div>
+	</div>
 	
+	<div class="fd">
+	    <div class='left_div'>
+	    <label for="link">
+		Display 'Powered by WisdmLabs'
+	    </label>
+	    </div>
+	    <div class='right_div'>
+	    <input type="checkbox" disabled class="wdm_wpi_input wdm_wpi_checkbox" value="1" id="show_powered_by_link" />
+		 <?php echo $pro; ?>
+	    </div>
+	<div class="clear"></div>
+	</div>
+	
+	<div class="fd">
+	    <div class='left_div'>
+	    <label for="enable_telephone_no_txtbox">
+		Display Telephone Number Field
+	    </label>
+	    </div>
+	    <div class='right_div'>
+		<input type="checkbox" disabled class="wdm_wpi_input wdm_wpi_checkbox" value="1" id="enable_telephone_no_txtbox" />
+		     <?php echo $pro; ?>
+	    </div>
+	<div class="clear"></div>
+	</div>
+	</fieldset>
+	    <br>
+	    <fieldset>
+		    <legend>Styling Options </legend>
+			<div class='fd'>
+				    <div class='left_div'>
+				    <label for="enable_telephone_no_txtbox">
+				    Custom Styling
+				   </label>
+				    </div>
+				    <div class='right_div'>
+				    <input type="radio" class="wdm_wpi_input wdm_wpi_checkbox" value="theme_css" name="wdm_radio_data" id="theme_css" checked />
+																					    <em> Use Activated Theme CSS </em><br>
+										 
+				    <input type="radio" class="wdm_wpi_input wdm_wpi_checkbox" value="manual_css" name="wdm_radio_data" id="manual_css" />
+					
+				    <em>Manually specify color settings</em>
+				  </div>
+				    <div class="clear"></div>
+			</div>
+	    </fieldset>  
+	   <br />
+	   <div name="Other_Settings" id="Other_Settings" style="display: none;">
+	    <fieldset style="padding: 0;">
+		<?php echo '<legend>'. __("Specify CSS Settings ",'pep_text_domain').'</legend>';?>
+	    <br />
+		<div class="layer_parent">
+		    <div class="pew_upgrade_layer">
+			<div class="pew_uptp_cont">
+			    <p> This feature is available in the PRO version. Click below to know more.</p>
+			<a class="wdm_view_det_link" href="http://wisdmlabs.com/woocommerce-product-enquiry-pro/" target="_blank"> View Details </a>
+			</div>
+		    </div>
+		    <img src="<?php echo plugins_url('img/buttons-css.png',  __FILE__);?>" />
+		</div>
+	    </fieldset>
+	    </div>
+      
         <p>
-            <input type="submit" class="wdm_wpi_input button-primary" value="Save changes" id="wdm_ask_button" />
+            <input type="submit" class="wdm_wpi_input button-primary" value="Save Changes" id="wdm_ask_button" />
         </p>
         </div>
-      
-    </form>
-
-    </div>
-      
-    <script type="text/javascript">
+	<br/>
+     </form>
+     
+         <script type="text/javascript">
 	jQuery(document).ready(
-			       function()
+			       function($)
 			       {
-					jQuery("#ask_product_form").validate();	
+					$("#ask_product_form").validate();
+					
+					if($("#manual_css").is(':checked')) {
+					    $("#Other_Settings").show();
+					}
+					else{
+					   $("#Other_Settings").hide(); 
+					}
+					
+					$("#theme_css").click(function(){$("#Other_Settings").hide();});
+					$("#manual_css").click(function(){$("#Other_Settings").show();});
 				}
 				);
     </script>
+	 
+     <?php } ?>
+    </div>
     
       <?php
       //add styles for settings page
@@ -155,26 +380,4 @@ function add_ask_product_settings()
     include_once('wisdm_sidebar/wisdm_sidebar.php');
     pew_create_wisdm_sidebar($plugin_name, $wdm_plugin_slug);
 }
-
-function pew_appeal_notice()
-{
-    if((isset($_REQUEST['page']) && $_REQUEST['page'] == 'product-enquiry-for-woocommerce') && (isset($_REQUEST['settings-updated']) && $_REQUEST['settings-updated'] == true))
-    {
-	$wdm_plugin_slug = 'product-enquiry-for-woocommerce';
-    
-	?>
-    
-	<div class="wdm_appeal_text" style="background-color:#FFE698;padding:10px;margin-right:10px;">
-	    <strong>An Appeal:</strong>
-	    We strive hard to bring you useful, high quality plugins for FREE and to provide prompt responses to all your support queries.
-	    If you are happy with our work, please consider making a good faith donation, here -
-	    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info%40wisdmlabs%2ecom&lc=US&item_name=WisdmLabs%20Plugin%20Donation&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest" target="_blank"> Donate now</a> 
-	    and do post an encouraging review, here - <a href="http://wordpress.org/support/view/plugin-reviews/<?php echo $wdm_plugin_slug; ?>" target="_blank"> Review this plugin</a>.
-	</div>
-    
-	<?php
-    }
-}
-
-add_action('admin_notices', 'pew_appeal_notice');
 ?>
--- readme.txt
+++ readme.txt
@@ -4,9 +4,9 @@ Contributors: WisdmLabs
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info%40wisdmlabs%2ecom&lc=US&item_name=WisdmLabs%20Plugin%20Donation&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest
 Tags: WisdmLabs, WooCommerce, Product Enquiry, Product Enquiry for WooCommerce, eCommerce Product Enquiry, Ask Question about a Product, WooCommerce Plugin, WooCommerce Product Plugin
 Requires at least: 3.4
-Tested up to: 3.5.1
-Version: 0.2.9
-Stable tag: 0.2.9
+Tested up to: 3.8.3
+Version: 0.3
+Stable tag: 0.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -14,13 +14,30 @@ Product Enquiry for WooCommerce allows prospective customers or visitors to make
 
 == Description ==
 
-* This plugin allows customers or visitors to make enquiry about a product by asking question about a product directly from the product listing page.
+The Product Enquiry for WooCommerce plugin adds an enquiry button to every WooCommerce Product Page. Using this button a potential customer, can send you an enquiry about the product he is unsure of purchasing. By addressing his enquiries, you can convert an unsure customer, into a buyer! 
 
-* Plugin user/administrator can configure where to show "Enquiry Button" on the product page and many other details too(please, see screenshot 'Plugin configuration' for more details).
+Also check out our premium version, <a href="http://wisdmlabs.com/woocommerce-product-enquiry-pro/">Product Enquiry PRO for WooCommerce</a>. It allows you to view enquiries right in your dashboard. You can even filter and export enquiries to help you analyse product demands.
 
-* An email is sent to pre-configured email address of the plugin user with details such as site name, product name, product URL, customer name and enquiry message in a well formatted manner. Moreover, the customer can receive a copy of this email if this functionality is configured to be allowed by the plugin administrator.
+= Features of Product Enquiry for WooCommerce =
+* Add an Enquiry Button: Allows customers or visitors to make enquiry about a product by asking question about a product directly from the product listing page.
+* Configure Settings: Plugin user/administrator can configure where to show "Enquiry Button" on the product page and many other details too(please, see screenshot 'Plugin configuration' for more details).
+* Email to Site Owner: An email is sent to pre-configured email address of the plugin user with details such as site name, product name, product URL, customer name and enquiry message in a well formatted manner.
+* Moreover, the customer can receive a copy of this email if this functionality is configured to be allowed by the plugin administrator.
+* Contact Us: A plugin support sidebar in settings page provides you options to request a support query, make an enquiry for custom development, know additional services provided by us etc. right from the plugin settings page.
 
-* You will find a plugin support sidebar in settings page: This sidebar enables various features such as asking a support query, making enquiry for custom development, know services provided by us etc. right from the plugin settings page.
+
+Our premium version enhances the features of Product Enquiry for WooCommerce, and provides a systematic approach to maintain and filter enquiries.
+= Additional Features in PRO =
+* View Enquiries in Dashboard: You can view enquiries made directly in your dashboard.
+* Filter and Export Enquiry Records: Filter and export enquiry records based on Product Name, Customer Name, etc. to analyse product demands.
+* Style your Enquiry Button and Dialog: Customize the look of your enquiry button and dialog, or use the theme's default CSS settings.
+* Auto-inform Product Owner: The enquiry emails are sent to the site owner as well as the product owner/seller.
+* Responsive Enquiry Dialog: The enquiry button and dialog are responsive, to help customer's seamlessly make enquiries form a mobile device.
+* Field for Telephone Number: An additional (optional) field is provided to input the customer's telephone number.
+* Localization Ready: The PRO version is ready for translation. All you need to do is create the .mo and .po files for your language. Or you can contact us to help you.
+* Auto Fill form fields: If a customer makes regular enquiries, the form autofills the customer's data, saving the customer's time.
+
+<a href="http://wisdmlabs.com/woocommerce-product-enquiry-pro/">Get Product Enquiry PRO for WooCommerce</a>.
 
 == Installation ==
 
@@ -37,6 +54,11 @@ As this is an extension for the WooCommerce plugin, you must have WooCommerce pl
 3. Product enquiry form
 
 == Changelog ==
+
+= 0.3 =
+* Fixed z-index issue.
+* Resolved IE issue.
+* Updated plugin dashboard UI.
 
 = 0.2.9 =
 * Fixed enquiry button alignment issue.
